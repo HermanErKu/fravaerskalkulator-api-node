44796013ba49eb257861a38b6cb9d65f0140ecd6
Herman Lindalen Nodeland <afs0027473@agderskolen.no>
2024-10-18 15:43:46 +0200

    added basic funcionality to get utdanningsprogrammer

--- index.js
+++ index.js
@@ -21,7 +21,63 @@ const db = new sqlite3.Database('./database.db');
 
 app.use(express.json());
 
-app.post("/msg", (req, res, next) => {
- const message = req.body.message;
- res.json({"receivedMessage": message});
+
+//// utdanningspogrammer endpoints
+db.run("CREATE TABLE IF NOT EXISTS utdanningsprogrammer (id INTEGER PRIMARY KEY AUTOINCREMENT, navn STRING, forkortelse STRING);")
+
+// empty endpoints
+app.get("/api", (req, res, next) => {
+    db.all("SELECT * FROM utdanningsprogrammer", (err, rows) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+app.post("/api", (req, res, next) => {
+    const message = req.body;
+    console.log(message);
+    db.run('INSERT INTO utdanningsprogrammer (navn, forkortelse) VALUES (?, ?)', message.navn, message.forkortelse, (err) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json({ "id": this.lastID });
+    });
+});
+
+
+
+// ID specific endpoints
+app.get("/api/:id", (req, res, next) => {
+    db.all("SELECT * FROM utdanningsprogrammer WHERE id = ?", req.params.id, (err, rows) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+app.put("/api/:id", (req, res, next) => {
+    const message = req.body;
+    db.run('UPDATE utdanningsprogrammer SET navn = ?, forkortelse = ? WHERE id = ?', message.navn, message.forkortelse, req.params.id, (err) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json({ "id": this.lastID });
+    });
+});
+
+app.delete("/api/:id", (req, res, next) => {
+    db.run('DELETE FROM utdanningsprogrammer WHERE id = ?', req.params.id, (err) => {
+        if (err) {
+            res.status(500).json({ "error": err.message });
+            return;
+        }
+        res.json({ "deleted": this.changes });
+    });
 });
